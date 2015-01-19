94f725c2d8171162411cbbedd32100acb0243500
Justin Zimmerman <zimmy130@gmail.com>
2015-01-19 14:59:29 -0500

    add degree and radian functions

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 //http://en.wikipedia.org/wiki/Vincenty%27s_formulae
 //http://www.movable-type.co.uk/scripts/latlong-vincenty.html#direct
+
+
+
+function toRad(degree) {
+ return degree * Math.PI / 180;
+};
+function toDeg(radian) {
+ return radian * 180 / Math.PI;
+};
